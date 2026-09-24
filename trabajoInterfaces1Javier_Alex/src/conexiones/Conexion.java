d314806f81aa475e426c07b2833268ec11f2e662
alex.musca07-cloud <musca@Katy>
2026-09-24 18:57:37 +0200

    Conexion puesta con env

--- trabajoInterfaces1Javier_Alex/src/conexiones/Conexion.java
+++ trabajoInterfaces1Javier_Alex/src/conexiones/Conexion.java
@@ -9,10 +9,10 @@ public class Conexion {
 	public static Connection getConnection() {
 		try {
 
-			String user = "root";
-			String pwd = "";
-			String url = "jdbc:mysql://localhost:3306/interfaces";
-			Connection con = DriverManager.getConnection(url, user, pwd);
+			String url = System.getenv("DB_URL");
+			String user = System.getenv("DB_USER");
+			String pass = System.getenv("DB_PASS");
+			Connection con = DriverManager.getConnection(url, user, pass);
 			return con;
 		} catch (SQLException e) {
 
